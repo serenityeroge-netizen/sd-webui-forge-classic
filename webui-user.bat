7b880bc30cc41576620e6ce488438e0a048df0e2
serenityeroge-netizen <serenityeroge@gmail.com>
2026-09-07 15:05:50 -0400

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -4,7 +4,7 @@
 :: set GIT=
 :: set VENV_DIR=
 
-set COMMANDLINE_ARGS=  --lowvram  --dISable-ck-attention --disable-gpu-warning  --model-ref "D:\sd-webui-forge-classic-2.25\models"
+set COMMANDLINE_ARGS=  --lowvram  --disable-gpu-warning  --model-ref "D:\sd-webui-forge-classic-2.25\models"
 :: --xformers --sage --uv
 :: --pin-shared-memory --cuda-malloc --cuda-stream
 :: --skip-python-version-check --skip-torch-cuda-test --skip-version-check --skip-prepare-environment --skip-install
